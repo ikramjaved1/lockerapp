a6a0bf34b203db27a8eef6a42824ef15ee344d79
Ikram Javad <ikram.javad@xprolabs.com>
2022-06-28 17:55:33 +0500

    Product: improve indentation

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,7 +5,7 @@ class HomesController < ApplicationController
 
   def profile
     @user = User.find(params[:id])
-    @products=Product.where("user_id=?", current_user.id)
+    @products = Product.where("user_id=?", current_user.id)
   end
 
   def switch_role
