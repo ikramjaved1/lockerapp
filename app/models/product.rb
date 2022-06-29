be7384435bb9df084eede16814ba93b4c67c518c
Ikram Javad <ikram.javad@xprolabs.com>
2022-06-29 15:37:00 +0500

    Product: added options in product.rb and improve indentation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_one_attached :product_image
+  has_many :comments, dependent: :destroy
+  has_one_attached :product_image , dependent: :destroy
 end
